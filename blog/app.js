bd2b1ac213f77dd7c06e9ad9e8ce28a0d2f6bd0c
1823yangxiaoyun <1762098303@qq.com>
2019-11-12 19:48:00 +0800

    second

--- blog/app.js
+++ blog/app.js
@@ -5,10 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 
 var app = express();
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,10 +16,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/images')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,6 +35,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-// app.listen(8080)
 
 module.exports = app;
--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,28 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var data=require('./data.json');
-router.use(express.static(__dirname));
 
 router.get("/add",function(req,res){
   res.send(data);
 })
 router.get('/login', function(req, res) {
-  res.type("text/html");
-  res.status(200);
-  res.sendFile(__dirname+'/login.html')
+  res.render("login");
 });
 router.get('/list', function(req, res){
   var arr=data.users;
   // console.log(req.query);
   // console.log(arr[0].username);
   if(req.query.username ===arr[0].username && req.query.pwd===arr[0].password){
-    res.type("text/html");
-    res.status(200);
-    res.sendFile(__dirname+'/list.html');
+    res.render('list');
     return;
    }
   else{
-    res.send("用户名或密码错误！！")
+    res.send("用户名或密码错误！！请重新登录")
    }
 
 });
